ff68cc4160d3e0276670a12f1d1272c36fecaead
Lucas Sá <lucazartu@gmail.com>
2023-12-27 16:48:47 -0300

    feat: inclusão de pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <h1>
+      Na vida, mais importante do que de um dia pro outro se tornar especialista
+      em algo, é evoluir aos poucos e melhor ainda é sentir que essa evolução.
+      Você consegue! :D Agda Olívia Baía de Sá, eu te amo mais que tudo! Se você
+      me ama, vem cá me dar um beijinho :D
+    </h1>
+  );
+}
+
+export default Home;
